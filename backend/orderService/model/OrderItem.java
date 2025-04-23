847db23a123da29b270f085ce92e8cf1d481301e
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-04-23 21:40:15 +0300

    Create OrderItem.java

---
+++ backend/orderService/model/OrderItem.java
@@ -0,0 +1,40 @@
+package com.velixa.orderservice.model;
+
+import java.math.BigDecimal;
+
+public class OrderItem {
+    private String productId;
+    private String productName;
+    private int quantity;
+    private BigDecimal price;
+
+    // Getters and setters
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDec
