2fde2350a1207d0043248ad870a8299c37647724
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-04-23 21:34:09 +0300

    Create CartRepository.java

---
+++ backend/cartService/repository/CartRepository.java
@@ -0,0 +1,12 @@
+package com.velixa.cartservice.repository;
+
+import com.velixa.cartservice.model.Cart;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends MongoRepository<Cart, String> {
+    Optional<Cart> findByUserId(String userId);
+}
