7b64d2a099623b0d8e03988ec2264cf50ff1cb4e
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-04-30 02:32:30 +0300

    Create OrderDTO.java

---
+++ backend/order-service/src/main/java/dto/OrderDTO.java
@@ -0,0 +1,52 @@
+package com.velixa.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDTO {
+    private String id;
+    private String userId;
+    private List<String> productIds;
+    private Double totalPrice;
+    
+    
+	public OrderDTO(String id, String userId, List<String> productIds, Double totalPrice) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.productIds = productIds;
+		this.totalPrice = totalPrice;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public List<String> getProductIds() {
+		return productIds;
+	}
+	public void setProductIds(List<String> productIds) {
+		this.productIds = productIds;
+	}
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+    
+    
+}
